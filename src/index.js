e4957990ea2e240b02a5b63f356fef5cce7dbfe2
SergeyBaranovichi <gsergey1998@gmail.com>
2018-10-07 21:17:40 +0300

    longest-consecutive-sequence

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-  var res=0;
-  if (array.length>0){ 
-    res=1;
-    var n = array.length;
-    for (var i = 0; i < n; i++){ 
-      var v = array[ i ], j = i-1;
-      while (j >= 0 && array[j] > v){ 
-        array[j+1] = array[j]; 
-        j--; 
-        }
-        array[j+1] = v;
-      }                    
-    for (var i=0; i<array.length-1; i++){ 
-      var n=1;
-        while (array[i+1]-array[i]==1 || array[i+1]==array[i]) {
-          n++;
-          if (array[i+1]==array[i]) n--;
-          i++;
-          }
-        res=Math.max(res,n);  
-      }
+// your solution here
+  array.sort(compare);
+  if (array.length == 0) {
+    return 0;
     }
-  return res;
+  var c = 1;
+  var res = 1;
+   function compare(a,b){
+    if(a>b) return 1;
+    if(a<b) return -1;
+    }
+  for (var i=0; i<array.length; i++){
+    if(array[i]+1 == array[i+1]){
+      c++;
+      if(c>res){
+        res=c;
+        }
+      } else if(array[i] == array[i+1]){
+        c=c;
+        } else c=1;
+      }
+    return res;
   }
